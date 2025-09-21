da7f1729496131db09e3add87937bc392a100c9e
Gustavo Alessandro <gualessandro2017@gmail.com>
2025-09-21 18:48:56 -0300

    Usuário 2: Adicionado método MD3() à classe D

--- D.java
+++ D.java
@@ -24,4 +24,8 @@ public class D {
     public void MD2() {
         System.out.println("Método MD2 executado");
     }
+
+    public void MD3() {
+        System.out.println("Método MD3 executado - Usuário 1");
+    }
 }
